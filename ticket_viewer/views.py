51af9868d741797c61d0691c9238e2ce76e4c061
sakpal <sakpals@student.unimelb.edu.au>
2018-06-10 15:52:54 +1000

    displays ticket list

--- ticket_viewer/views.py
+++ ticket_viewer/views.py
@@ -11,3 +11,14 @@ token = 'rmgm9bSehKgL09Eh2DJC98PNFZrgjOYqPQwzX0Zf'
 def main():
 	response = requests.get(url+'/tickets.json', auth=(user, token))
 	
+	if response.status_code != 200:
+		return render_template('ticket_disp_error.html')
+	
+	data = response.json()
+	
+	return render_template('ticket_list.html', ticket_list=data['tickets'])
+	
+@app.route('/ticket/<ticket_id>') #/ticket/<id>
+def single_ticket(ticket_id):
+	print(ticket_id)
+	return render_template('ticket_full_details.html')
