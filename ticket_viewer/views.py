ef22fafda12a18613f153725bf5255df63e18c8a
sakpal <sakpals@student.unimelb.edu.au>
2018-06-10 18:49:42 +1000

    can select and view a ticket

--- ticket_viewer/views.py
+++ ticket_viewer/views.py
@@ -10,15 +10,17 @@ token = 'rmgm9bSehKgL09Eh2DJC98PNFZrgjOYqPQwzX0Zf'
 @app.route('/')
 def main():
 	response = requests.get(url+'/tickets.json', auth=(user, token))
-	
 	if response.status_code != 200:
 		return render_template('ticket_disp_error.html')
 	
 	data = response.json()
-	
 	return render_template('ticket_list.html', ticket_list=data['tickets'])
 	
 @app.route('/ticket/<ticket_id>') #/ticket/<id>
 def single_ticket(ticket_id):
-	print(ticket_id)
-	return render_template('ticket_full_details.html')
+	response = requests.get(url+'/tickets/'+str(ticket_id)+'.json', auth=(user, token))
+	if response.status_code != 200:
+		return render_template('ticket_disp_error.html') #probs customise it
+	
+	data = response.json()
+	return render_template('ticket_full_details.html', ticket_info=data['ticket'])
