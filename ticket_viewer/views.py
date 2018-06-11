636a0e12621103719691332209c9ac25b2f3798c
sakpal <sakpals@student.unimelb.edu.au>
2018-06-11 21:41:40 +1000

    implemented pagination

--- ticket_viewer/views.py
+++ ticket_viewer/views.py
@@ -5,7 +5,7 @@ import requests, os, sys
 url = 'https://katsucat1.zendesk.com/api/v2'
 user = 'sampadasakpal@hotmail.com'+'/token'
 token = 'rmgm9bSehKgL09Eh2DJC98PNFZrgjOYqPQwzX0Zf'
-
+curr_page_num = 1
 
 def check_internet_connection():
 	# check for internet connectivity
@@ -13,28 +13,26 @@ def check_internet_connection():
 		return render_template('internet_disconnect.html')
 
 @app.route('/')
-def main():
+def ticket_list():
 	check_internet_connection()
-
-	if os.system("ping -c 5 google.com") == 1:
-		return render_template('internet_disconnect.html')
-
-	url = 'https://katsucat1.zendesk.com/api/v2/tickets.json'
-	while url:
-		response = requests.get(url+'?per_page=25', auth=(user, token))
-		if response.status_code != 200:
+	response = requests.get(url+'/tickets/?page=1&per_page=25', auth=(user, token))
+	if response.status_code != 200:
 			return render_template('ticket_disp_error.html')
 	
-		data = response.json()
-		url = data['next_page']
-		return render_template('ticket_list.html', ticket_list=data['tickets'])
+	data = response.json()
+	return render_template('ticket_list.html', ticket_list=data['tickets'],
+		next_pg=data['next_page'], prev_pg=data['previous_page'], page_num=curr_page_num)
+
+@app.route('/page/<page_num>')
+def ticket_list_page(page_num):
+	curr_page_num = int(page_num)
+	response = requests.get(url+'/tickets/?page='+str(curr_page_num)+'&per_page=25', auth=(user, token))
+	if response.status_code != 200:
+			return render_template('ticket_disp_error.html')
 	
-	#response = requests.get(url+'/tickets.json', auth=(user, token))
-	#if response.status_code != 200:
-	#	return render_template('ticket_disp_error.html')
-	
-	#data = response.json()
-	#return render_template('ticket_list.html', ticket_list=data['tickets'])
+	data = response.json()
+	return render_template('ticket_list.html', ticket_list=data['tickets'],
+		next_pg=data['next_page'], prev_pg=data['previous_page'], page_num=curr_page_num)
 
 @app.route('/ticket/<ticket_id>')
 def single_ticket(ticket_id):
